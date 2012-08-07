e421e9c9b4b75dab82dc55d4dbeff3955cfae2e2
Brian Chiu <brian.chiu@liferay.com>
2012-08-07 12:06:59 -0700

    Test Fixes - portlet/documentsandmedia

--- portal-web/test/com/liferay/portalweb/portlet/documentsandmedia/dmdocument/adddmfolderdocumentnull/AddDMFolderDocumentNullTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/documentsandmedia/dmdocument/adddmfolderdocumentnull/AddDMFolderDocumentNullTest.java
@@ -24,23 +24,6 @@ public class AddDMFolderDocumentNullTest extends BaseTestCase {
 	public void testAddDMFolderDocumentNull() throws Exception {
 		selenium.open("/web/guest/home/");
 		loadRequiredJavaScriptModules();
-
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
-
-			try {
-				if (selenium.isVisible("link=Documents and Media Test Page")) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
-
-			Thread.sleep(1000);
-		}
-
 		selenium.clickAt("link=Documents and Media Test Page",
 			RuntimeVariables.replace("Documents and Media Test Page"));
 		selenium.waitForPageToLoad("30000");
@@ -84,7 +67,7 @@ public class AddDMFolderDocumentNullTest extends BaseTestCase {
 
 			try {
 				if (selenium.isVisible(
-							"//div[@class='lfr-component lfr-menu-list']/ul/li[4]/a")) {
+							"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Basic Document')]/a")) {
 					break;
 				}
 			}
@@ -96,8 +79,8 @@ public class AddDMFolderDocumentNullTest extends BaseTestCase {
 
 		assertEquals(RuntimeVariables.replace("Basic Document"),
 			selenium.getText(
-				"//div[@class='lfr-component lfr-menu-list']/ul/li[4]/a"));
-		selenium.clickAt("//div[@class='lfr-component lfr-menu-list']/ul/li[4]/a",
+				"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Basic Document')]/a"));
+		selenium.clickAt("//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Basic Document')]/a",
 			RuntimeVariables.replace("Basic Document"));
 		selenium.waitForPageToLoad("30000");
 		loadRequiredJavaScriptModules();
