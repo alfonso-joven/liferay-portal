b358e1272d9eaee9684ae5b5f16036b942832772
Albert Lee <albert.lee@liferay.com>
2012-08-23 07:38:39 -0700

    Test Fixes - portal/controlpanel/pagetemplates

--- portal-web/test/com/liferay/portalweb/portal/controlpanel/pagetemplates/pagetemplate/activatepagetemplate/ActivatePageTemplateTest.java
+++ portal-web/test/com/liferay/portalweb/portal/controlpanel/pagetemplates/pagetemplate/activatepagetemplate/ActivatePageTemplateTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portalweb.portal.controlpanel.pagetemplates;
+package com.liferay.portalweb.portal.controlpanel.pagetemplates.pagetemplate.activatepagetemplate;
 
 import com.liferay.portalweb.portal.BaseTestCase;
 import com.liferay.portalweb.portal.util.RuntimeVariables;
@@ -23,6 +23,26 @@ import com.liferay.portalweb.portal.util.RuntimeVariables;
 public class ActivatePageTemplateTest extends BaseTestCase {
 	public void testActivatePageTemplate() throws Exception {
 		selenium.open("/web/guest/home/");
+		selenium.clickAt("//div[@id='dockbar']",
+			RuntimeVariables.replace("Dockbar"));
+
+		for (int second = 0;; second++) {
+			if (second >= 90) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent(
+							"//script[contains(@src,'/aui/aui-editable/aui-editable-min.js')]")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		assertEquals(RuntimeVariables.replace("Go to"),
 			selenium.getText("//li[@id='_145_mySites']/a/span"));
 		selenium.mouseOver("//li[@id='_145_mySites']/a/span");
@@ -49,14 +69,14 @@ public class ActivatePageTemplateTest extends BaseTestCase {
 		selenium.clickAt("link=Page Templates",
 			RuntimeVariables.replace("Page Templates"));
 		selenium.waitForPageToLoad("30000");
-		assertEquals(RuntimeVariables.replace("Test Page Template"),
-			selenium.getText("//tr[contains(.,'Test Page Template')]/td[1]/a"));
+		assertEquals(RuntimeVariables.replace("Page Template Name"),
+			selenium.getText("//tr[contains(.,'Page Template Name')]/td[1]/a"));
 		assertEquals(RuntimeVariables.replace("No"),
-			selenium.getText("//tr[contains(.,'Test Page Template')]/td[2]/a"));
+			selenium.getText("//tr[contains(.,'Page Template Name')]/td[2]/a"));
 		assertEquals(RuntimeVariables.replace("Actions"),
 			selenium.getText(
-				"//tr[contains(.,'Test Page Template')]/td/span[@title='Actions']/ul/li/strong/a/span"));
-		selenium.clickAt("//tr[contains(.,'Test Page Template')]/td/span[@title='Actions']/ul/li/strong/a/span",
+				"//tr[contains(.,'Page Template Name')]/td/span[@title='Actions']/ul/li/strong/a/span"));
+		selenium.clickAt("//tr[contains(.,'Page Template Name')]/td/span[@title='Actions']/ul/li/strong/a/span",
 			RuntimeVariables.replace("Actions"));
 
 		for (int second = 0;; second++) {
@@ -89,29 +109,12 @@ public class ActivatePageTemplateTest extends BaseTestCase {
 		selenium.clickAt("//input[@value='Save']",
 			RuntimeVariables.replace("Save"));
 		selenium.waitForPageToLoad("30000");
-
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
-
-			try {
-				if (selenium.isVisible("//div[@class='portlet-msg-success']")) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
-
-			Thread.sleep(1000);
-		}
-
 		assertEquals(RuntimeVariables.replace(
 				"Your request completed successfully."),
 			selenium.getText("//div[@class='portlet-msg-success']"));
-		assertEquals(RuntimeVariables.replace("Test Page Template"),
-			selenium.getText("//tr[contains(.,'Test Page Template')]/td[1]/a"));
+		assertEquals(RuntimeVariables.replace("Page Template Name"),
+			selenium.getText("//tr[contains(.,'Page Template Name')]/td[1]/a"));
 		assertEquals(RuntimeVariables.replace("Yes"),
-			selenium.getText("//tr[contains(.,'Test Page Template')]/td[2]/a"));
+			selenium.getText("//tr[contains(.,'Page Template Name')]/td[2]/a"));
 	}
 }
