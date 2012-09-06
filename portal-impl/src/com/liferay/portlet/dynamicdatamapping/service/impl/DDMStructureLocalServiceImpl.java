5dca3c4fe0462db08a80b9639fdd68b55bb176ce
Brian Chan <brian.chan@liferay.com>
2012-09-05 23:18:46 -0700

    LPS-28756 Source formatting

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMStructureLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMStructureLocalServiceImpl.java
@@ -52,7 +52,6 @@ import com.liferay.portlet.dynamicdatamapping.util.DDMXMLUtil;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
