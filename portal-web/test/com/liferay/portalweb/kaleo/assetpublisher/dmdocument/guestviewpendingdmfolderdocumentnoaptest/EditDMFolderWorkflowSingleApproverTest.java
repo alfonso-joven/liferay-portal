839827cf0e15a4743e81f3799025eba72e17f22e
Albert Lee <albert.lee@liferay.com>
2012-09-12 08:51:02 -0700

    LPS-10916 Test Formatting

--- portal-web/test/com/liferay/portalweb/kaleo/assetpublisher/dmdocument/guestviewpendingdmfolderdocumentnoaptest/EditDMFolderWorkflowSingleApproverTest.java
+++ portal-web/test/com/liferay/portalweb/kaleo/assetpublisher/dmdocument/guestviewpendingdmfolderdocumentnoaptest/EditDMFolderWorkflowSingleApproverTest.java
@@ -23,6 +23,8 @@ import com.liferay.portalweb.portal.util.RuntimeVariables;
 public class EditDMFolderWorkflowSingleApproverTest extends BaseTestCase {
 	public void testEditDMFolderWorkflowSingleApprover()
 		throws Exception {
+		selenium.selectWindow("null");
+		selenium.selectFrame("relative=top");
 		selenium.open("/web/guest/home/");
 		selenium.clickAt("link=Documents and Media Test Page",
 			RuntimeVariables.replace("Documents and Media Test Page"));
@@ -30,35 +32,24 @@ public class EditDMFolderWorkflowSingleApproverTest extends BaseTestCase {
 		Thread.sleep(5000);
 		selenium.clickAt("//span[@class='overlay document-action']/span/ul/li/strong/a",
 			RuntimeVariables.replace("Actions"));
-
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
-
-			try {
-				if (selenium.isVisible(
-							"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Edit')]/a")) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
-
-			Thread.sleep(1000);
-		}
-
+		selenium.waitForVisible(
+			"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Edit')]/a");
 		assertEquals(RuntimeVariables.replace("Edit"),
 			selenium.getText(
 				"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Edit')]/a"));
 		selenium.click(RuntimeVariables.replace(
 				"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Edit')]/a"));
 		selenium.waitForPageToLoad("30000");
+		assertEquals(RuntimeVariables.replace("Home"),
+			selenium.getText("//h1[@class='header-title']/span"));
 		assertEquals(RuntimeVariables.replace(
 				"Default Workflow for all Document Types"),
 			selenium.getText("//label[@for='workflowDefinition-1']"));
 		selenium.select("//select[@id='_20_workflowDefinition-1']",
 			RuntimeVariables.replace("Single Approver (Version 1)"));
+		assertEquals("Single Approver (Version 1)",
+			selenium.getSelectedLabel(
+				"//select[@id='_20_workflowDefinition-1']"));
 		selenium.clickAt("//input[@value='Save']",
 			RuntimeVariables.replace("Save"));
 		selenium.waitForPageToLoad("30000");
