285ec147d70bd7b404b893d0879692f44d80ea99
Hugo Huijser <hugo.huijser@liferay.com>
2012-09-19 08:55:21 -0700

    Revert "LPS-28728 Source formatting"

--- portal-impl/src/com/liferay/portal/search/lucene/LuceneIndexSearcherImpl.java
+++ portal-impl/src/com/liferay/portal/search/lucene/LuceneIndexSearcherImpl.java
@@ -563,7 +563,11 @@ public class LuceneIndexSearcherImpl implements IndexSearcher {
 
 			QueryConfig queryConfig = query.getQueryConfig();
 
-			for (int i = start; i < start + subsetTotal; i++) {
+			for (int i = start; i < end; i++) {
+				if (i >= PropsValues.INDEX_SEARCH_LIMIT) {
+					break;
+				}
+
 				int docId = hitDocs.getDocId(i);
 
 				org.apache.lucene.document.Document document =
@@ -583,6 +587,9 @@ public class LuceneIndexSearcherImpl implements IndexSearcher {
 						document, query, Field.DESCRIPTION, locale, queryTerms);
 					getSnippet(
 						document, query, Field.TITLE, locale, queryTerms);
+				}
+				else {
+					queryTerms = getQueryTerms(query);
 				}
 
 				subsetDocument.addText(Field.SNIPPET, subsetSnippet);
@@ -609,10 +616,6 @@ public class LuceneIndexSearcherImpl implements IndexSearcher {
 					catch (Exception e) {
 					}
 				}
-			}
-
-			if (!queryConfig.isHighlightEnabled()) {
-				queryTerms = getQueryTerms(query);
 			}
 
 			hits.setStart(startTime);
