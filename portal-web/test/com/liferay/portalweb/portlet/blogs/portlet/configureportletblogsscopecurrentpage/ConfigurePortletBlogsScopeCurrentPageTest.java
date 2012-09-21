890a3b2728c9953fd025dadc82258bdc105e24d2
Ken Duenwald <ken.duenwald@liferay.com>
2012-09-21 15:04:18 -0700

    Test Fixes - portlet/blogs

--- portal-web/test/com/liferay/portalweb/portlet/blogs/portlet/configureportletblogsscopecurrentpage/ConfigurePortletBlogsScopeCurrentPageTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/blogs/portlet/configureportletblogsscopecurrentpage/ConfigurePortletBlogsScopeCurrentPageTest.java
@@ -29,7 +29,9 @@ public class ConfigurePortletBlogsScopeCurrentPageTest extends BaseTestCase {
 		selenium.clickAt("link=Blogs Test Page",
 			RuntimeVariables.replace("Blogs Test Page"));
 		selenium.waitForPageToLoad("30000");
-		Thread.sleep(5000);
+		selenium.waitForElementPresent(
+			"//script[contains(@src,'/liferay/navigation_interaction.js')]");
+		selenium.waitForVisible("//span[@title='Options']/ul/li/strong/a");
 		assertEquals(RuntimeVariables.replace("Options"),
 			selenium.getText("//span[@title='Options']/ul/li/strong/a"));
 		selenium.clickAt("//span[@title='Options']/ul/li/strong/a",
