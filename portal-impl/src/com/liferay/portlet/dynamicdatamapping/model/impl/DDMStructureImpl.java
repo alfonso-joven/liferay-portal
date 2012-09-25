0b07ff23fce60876366af1d67dc9ec9f45a611d9
Minhchau Dang <minhchau.dang@liferay.com>
2012-09-25 16:24:41 -0700

    LPS-30163 Mark _localizedFieldsMap as a CacheField

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/model/impl/DDMStructureImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/model/impl/DDMStructureImpl.java
@@ -206,6 +206,10 @@ public class DDMStructureImpl extends DDMStructureBaseImpl {
 		return getFieldProperty(fieldName, "type");
 	}
 
+	public Map<String, Map<String, Map<String, String>>> getLocalizedFieldsMap() {
+		return _localizedFieldsMap;
+	}
+
 	public List<DDMTemplate> getTemplates() throws SystemException {
 		return DDMTemplateLocalServiceUtil.getTemplates(getStructureId());
 	}
@@ -325,6 +329,7 @@ public class DDMStructureImpl extends DDMStructureBaseImpl {
 	@CacheField
 	private Document _document;
 
+	@CacheField
 	private Map<String, Map<String, Map<String, String>>> _localizedFieldsMap =
 		new ConcurrentHashMap<String, Map<String, Map<String, String>>>();
 
