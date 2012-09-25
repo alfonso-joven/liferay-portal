e42a6dc8166061b3a99dc7c6efec1841a7c2061d
Kenneth Chang <kenneth.chang@liferay.com>
2012-09-25 09:40:38 -0700

    LPS-30079 Column alias not allowed in Sybase subqueries

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -2192,7 +2192,7 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 
 		Projection plidProjection = ProjectionFactoryUtil.property("plid");
 
-		layoutDynamicQuery.setProjection(plidProjection);
+		layoutDynamicQuery.setNoColumnAliasProjection(plidProjection);
 
 		Property groupIdProperty = PropertyFactoryUtil.forName("groupId");
 
