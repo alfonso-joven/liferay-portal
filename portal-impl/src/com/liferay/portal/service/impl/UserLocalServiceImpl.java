58970a175c88d5173ca8cd5e8338b0911ebd8c39
Brian Chan <brian.chan@liferay.com>
2012-09-27 11:09:55 -0700

    LPS-30112 Source formatting

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -133,7 +133,6 @@ import java.awt.image.RenderedImage;
 import java.io.IOException;
 import java.io.Serializable;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
