01831c491e35023736c17658c81b7b3f4390a236
Ken Duenwald <ken.duenwald@liferay.com>
2012-10-01 12:23:01 -0700

    Test Fixes - portlet/unitconverter

--- portal-web/test/com/liferay/portalweb/portlet/unitconverter/unit/convertunit/ConvertUnitTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/unitconverter/unit/convertunit/ConvertUnitTest.java
@@ -25,7 +25,6 @@ public class ConvertUnitTest extends BaseTestCase {
 		selenium.selectWindow("null");
 		selenium.selectFrame("relative=top");
 		selenium.open("/web/guest/home/");
-		selenium.waitForVisible("link=Unit Converter Test Page");
 		selenium.clickAt("link=Unit Converter Test Page",
 			RuntimeVariables.replace("Unit Converter Test Page"));
 		selenium.waitForPageToLoad("30000");
