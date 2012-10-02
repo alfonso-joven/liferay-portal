764b69345d2a80dc14122ca3df554760a9dc7659
Catherine Lui <catherine.lui@liferay.com>
2012-10-02 12:26:16 -0700

    Test Fixes - portal/dbupgrade/sampledatalatest/blogs/pagescope

--- portal-web/test/com/liferay/portalweb/portal/dbupgrade/sampledatalatest/blogs/pagescope/AddPage2PortletBlogsPageScopeTest.java
+++ portal-web/test/com/liferay/portalweb/portal/dbupgrade/sampledatalatest/blogs/pagescope/AddPage2PortletBlogsPageScopeTest.java
@@ -43,7 +43,7 @@ public class AddPage2PortletBlogsPageScopeTest extends BaseTestCase {
 		selenium.waitForElementPresent(
 			"//script[contains(@src,'/aui/aui-live-search/aui-live-search-min.js')]");
 		selenium.waitForVisible("//input[@id='layout_configuration_content']");
-		selenium.typeKeys("//input[@id='layout_configuration_content']",
+		selenium.sendKeys("//input[@id='layout_configuration_content']",
 			RuntimeVariables.replace("b"));
 		selenium.waitForVisible("//div[@title='Blogs']/p/a");
 		selenium.clickAt("//div[@title='Blogs']/p/a",
