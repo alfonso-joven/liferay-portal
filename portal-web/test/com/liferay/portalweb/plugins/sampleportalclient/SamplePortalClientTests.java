a324b33d446bf34e5a5905d0b47ca5c55613f39d
Jeffrey Yang <jeffrey.yang@liferay.com>
2012-10-04 09:10:56 -0700

    Test Fixes - plugins/sampleportalclient

--- portal-web/test/com/liferay/portalweb/plugins/sampleportalclient/SamplePortalClientTests.java
+++ portal-web/test/com/liferay/portalweb/plugins/sampleportalclient/SamplePortalClientTests.java
@@ -15,6 +15,7 @@
 package com.liferay.portalweb.plugins.sampleportalclient;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.portal.util.TearDownPageTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -25,9 +26,9 @@ import junit.framework.TestSuite;
 public class SamplePortalClientTests extends BaseTestSuite {
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
-		testSuite.addTestSuite(AddPageTest.class);
-		testSuite.addTestSuite(AddPortletTest.class);
-		testSuite.addTestSuite(DeletePageTest.class);
+		testSuite.addTestSuite(AddPageSPCTest.class);
+		testSuite.addTestSuite(AddPortletSPCTest.class);
+		testSuite.addTestSuite(TearDownPageTest.class);
 
 		return testSuite;
 	}
