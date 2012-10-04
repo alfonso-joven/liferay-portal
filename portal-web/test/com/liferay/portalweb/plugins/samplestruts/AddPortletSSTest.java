f308fca72d1ed412a76603b1924cfde74108091c
Jeffrey Yang <jeffrey.yang@liferay.com>
2012-10-04 14:19:54 -0700

    Test Fixes - plugins/samplestruts

--- portal-web/test/com/liferay/portalweb/plugins/samplestruts/AddPortletSSTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/samplestruts/AddPortletSSTest.java
@@ -20,19 +20,33 @@ import com.liferay.portalweb.portal.util.RuntimeVariables;
 /**
  * @author Brian Wing Shun Chan
  */
-public class AddPortletTest extends BaseTestCase {
-	public void testAddPortlet() throws Exception {
+public class AddPortletSSTest extends BaseTestCase {
+	public void testAddPortletSS() throws Exception {
 		selenium.selectWindow("null");
 		selenium.selectFrame("relative=top");
 		selenium.open("/web/guest/home/");
 		selenium.waitForVisible("link=Sample Struts Test Page");
-		selenium.click(RuntimeVariables.replace("link=Sample Struts Test Page"));
+		selenium.clickAt("link=Sample Struts Test Page",
+			RuntimeVariables.replace("Sample Struts Test Page"));
 		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("//div[@id='dockbar']",
+			RuntimeVariables.replace("Dockbar"));
+		selenium.waitForElementPresent(
+			"//script[contains(@src,'/aui/aui-editable/aui-editable-min.js')]");
+		assertEquals(RuntimeVariables.replace("Add"),
+			selenium.getText("//li[@id='_145_addContent']/a/span"));
+		selenium.mouseOver("//li[@id='_145_addContent']/a/span");
+		selenium.waitForVisible("//a[@id='_145_addApplication']");
 		assertTrue(selenium.isPartialText("//a[@id='_145_addApplication']",
 				"More"));
 		selenium.clickAt("//a[@id='_145_addApplication']",
 			RuntimeVariables.replace("More"));
-		selenium.waitForElementPresent("//div[@title='Sample Struts']/p/a");
+		selenium.waitForElementPresent(
+			"//script[contains(@src,'/aui/aui-live-search/aui-live-search-min.js')]");
+		selenium.waitForVisible("//input[@id='layout_configuration_content']");
+		selenium.sendKeys("//input[@id='layout_configuration_content']",
+			RuntimeVariables.replace("s"));
+		selenium.waitForVisible("//div[@title='Sample Struts']/p/a");
 		selenium.clickAt("//div[@title='Sample Struts']/p/a",
 			RuntimeVariables.replace("Add"));
 		selenium.waitForVisible("//section");
