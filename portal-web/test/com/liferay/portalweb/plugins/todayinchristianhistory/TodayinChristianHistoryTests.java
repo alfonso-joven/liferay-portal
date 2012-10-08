23e939953db56b6edde739dcabe94469e535a980
Jeffrey Yang <jeffrey.yang@liferay.com>
2012-10-08 10:10:17 -0700

    Test Fixes - plugins/todayinchristianhistory

--- portal-web/test/com/liferay/portalweb/plugins/todayinchristianhistory/TodayinChristianHistoryTests.java
+++ portal-web/test/com/liferay/portalweb/plugins/todayinchristianhistory/TodayinChristianHistoryTests.java
@@ -15,6 +15,7 @@
 package com.liferay.portalweb.plugins.todayinchristianhistory;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.portal.util.TearDownPageTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -25,9 +26,9 @@ import junit.framework.TestSuite;
 public class TodayinChristianHistoryTests extends BaseTestSuite {
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
-		testSuite.addTestSuite(AddPageTest.class);
-		testSuite.addTestSuite(AddPortletTest.class);
-		testSuite.addTestSuite(DeletePageTest.class);
+		testSuite.addTestSuite(AddPageTICHTest.class);
+		testSuite.addTestSuite(AddPortletTICHTest.class);
+		testSuite.addTestSuite(TearDownPageTest.class);
 
 		return testSuite;
 	}
