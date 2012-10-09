3ad4e5b9c20458ad8bd4ac4c3e334d1f6c54673d
Jeffrey Yang <jeffrey.yang@liferay.com>
2012-10-09 12:02:55 -0700

    Test Fixes - plugins/westministercatechism

--- portal-web/test/com/liferay/portalweb/plugins/westminstercatechism/AddPortletWCTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/westminstercatechism/AddPortletWCTest.java
@@ -20,21 +20,33 @@ import com.liferay.portalweb.portal.util.RuntimeVariables;
 /**
  * @author Brian Wing Shun Chan
  */
-public class AddPortletTest extends BaseTestCase {
-	public void testAddPortlet() throws Exception {
+public class AddPortletWCTest extends BaseTestCase {
+	public void testAddPortletWC() throws Exception {
 		selenium.selectWindow("null");
 		selenium.selectFrame("relative=top");
 		selenium.open("/web/guest/home/");
 		selenium.waitForVisible("link=Westminster Catechism Test Page");
-		selenium.click(RuntimeVariables.replace(
-				"link=Westminster Catechism Test Page"));
+		selenium.clickAt("link=Westminster Catechism Test Page",
+			RuntimeVariables.replace("Westminster Catechism Test Page"));
 		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("//div[@id='dockbar']",
+			RuntimeVariables.replace("Dockbar"));
+		selenium.waitForElementPresent(
+			"//script[contains(@src,'/aui/aui-editable/aui-editable-min.js')]");
+		assertEquals(RuntimeVariables.replace("Add"),
+			selenium.getText("//li[@id='_145_addContent']/a/span"));
+		selenium.mouseOver("//li[@id='_145_addContent']/a/span");
+		selenium.waitForVisible("//a[@id='_145_addApplication']");
 		assertTrue(selenium.isPartialText("//a[@id='_145_addApplication']",
 				"More"));
 		selenium.clickAt("//a[@id='_145_addApplication']",
 			RuntimeVariables.replace("More"));
 		selenium.waitForElementPresent(
-			"//div[@title='Westminster Catechism']/p/a");
+			"//script[contains(@src,'/aui/aui-live-search/aui-live-search-min.js')]");
+		selenium.waitForVisible("//input[@id='layout_configuration_content']");
+		selenium.sendKeys("//input[@id='layout_configuration_content']",
+			RuntimeVariables.replace("w"));
+		selenium.waitForVisible("//div[@title='Westminster Catechism']/p/a");
 		selenium.clickAt("//div[@title='Westminster Catechism']/p/a",
 			RuntimeVariables.replace("Add"));
 		selenium.waitForVisible("//section");
