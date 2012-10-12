fda8446d3e6426796b5723f2a061a2b990da94ec
Jeffrey Yang <jeffrey.yang@liferay.com>
2012-10-12 12:19:45 -0700

    Test Fixes - plugins/samplegroovy

--- portal-web/test/com/liferay/portalweb/plugins/samplegroovy/SampleGroovyTests.java
+++ portal-web/test/com/liferay/portalweb/plugins/samplegroovy/SampleGroovyTests.java
@@ -15,6 +15,7 @@
 package com.liferay.portalweb.plugins.samplegroovy;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.portal.util.TearDownPageTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -25,9 +26,9 @@ import junit.framework.TestSuite;
 public class SampleGroovyTests extends BaseTestSuite {
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
-		testSuite.addTestSuite(AddPageTest.class);
-		testSuite.addTestSuite(AddPortletTest.class);
-		testSuite.addTestSuite(DeletePageTest.class);
+		testSuite.addTestSuite(AddPageSGTest.class);
+		testSuite.addTestSuite(AddPortletSGTest.class);
+		testSuite.addTestSuite(TearDownPageTest.class);
 
 		return testSuite;
 	}
