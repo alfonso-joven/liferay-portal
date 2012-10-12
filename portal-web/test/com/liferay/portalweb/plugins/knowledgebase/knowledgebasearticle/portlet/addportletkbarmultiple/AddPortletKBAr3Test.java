ad6d0cfb7b0f6c2b23903df803e17bfd36fbf10a
Jeffrey Yang <jeffrey.yang@liferay.com>
2012-10-12 10:12:46 -0700

    Test Fixes - plugins/knowledgebase

--- portal-web/test/com/liferay/portalweb/plugins/knowledgebase/knowledgebasearticle/portlet/addportletkbarmultiple/AddPortletKBAr3Test.java
+++ portal-web/test/com/liferay/portalweb/plugins/knowledgebase/knowledgebasearticle/portlet/addportletkbarmultiple/AddPortletKBAr3Test.java
@@ -43,12 +43,8 @@ public class AddPortletKBAr3Test extends BaseTestCase {
 		selenium.waitForElementPresent(
 			"//script[contains(@src,'/aui/aui-live-search/aui-live-search-min.js')]");
 		selenium.waitForVisible("//input[@id='layout_configuration_content']");
-		selenium.type("//input[@id='layout_configuration_content']",
+		selenium.sendKeys("//input[@id='layout_configuration_content']",
 			RuntimeVariables.replace("k"));
-		selenium.keyDown("//input[@id='layout_configuration_content']",
-			RuntimeVariables.replace("\\13"));
-		selenium.keyUp("//input[@id='layout_configuration_content']",
-			RuntimeVariables.replace("\\13"));
 		selenium.waitForElementPresent(
 			"//div[@title='Knowledge Base Article']/p/a");
 		selenium.clickAt("//div[@title='Knowledge Base Article']/p/a",
--- portal-web/test/com/liferay/portalweb/plugins/knowledgebase/knowledgebasedisplay/portlet/addportletkbdduplicate/AddPortletKBDDuplicateTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/knowledgebase/knowledgebasedisplay/portlet/addportletkbdduplicate/AddPortletKBDDuplicateTest.java
@@ -44,12 +44,8 @@ public class AddPortletKBDDuplicateTest extends BaseTestCase {
 		selenium.waitForElementPresent(
 			"//script[contains(@src,'/aui/aui-live-search/aui-live-search-min.js')]");
 		selenium.waitForVisible("//input[@id='layout_configuration_content']");
-		selenium.type("//input[@id='layout_configuration_content']",
+		selenium.sendKeys("//input[@id='layout_configuration_content']",
 			RuntimeVariables.replace("k"));
-		selenium.keyDown("//input[@id='layout_configuration_content']",
-			RuntimeVariables.replace("\\13"));
-		selenium.keyUp("//input[@id='layout_configuration_content']",
-			RuntimeVariables.replace("\\13"));
 		selenium.waitForVisible("//div[@title='Knowledge Base (Display)']");
 		assertFalse(selenium.isVisible(
 				"//div[@title='Knowledge Base (Display)']/p/a"));
