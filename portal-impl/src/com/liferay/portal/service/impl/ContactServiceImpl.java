a935c2d44471a1e82e43ff83991c643339c1c58b
Vilmos Papp <vilmos.papp@liferay.com>
2012-10-19 10:30:34 +0200

    LPS-30255 Implementation

--- portal-impl/src/com/liferay/portal/service/impl/ContactServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ContactServiceImpl.java
@@ -14,10 +14,31 @@
 
 package com.liferay.portal.service.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.Contact;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.service.base.ContactServiceBaseImpl;
+import com.liferay.portal.service.permission.UserPermissionUtil;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Vilmos Papp
  */
 public class ContactServiceImpl extends ContactServiceBaseImpl {
+
+	public Contact getContact(long contactId)
+		throws PortalException, SystemException {
+
+		Contact contact = contactPersistence.findByPrimaryKey(contactId);
+
+		User user = userPersistence.findByPrimaryKey(contact.getUserId());
+
+		UserPermissionUtil.check(
+			getPermissionChecker(), user.getUserId(), ActionKeys.VIEW);
+
+		return contact;
+	}
+
 }
