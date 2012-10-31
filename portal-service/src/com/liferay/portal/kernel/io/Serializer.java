5c155e7ddc3698e1c958af05e825b87517597c50
Brian Chan <brian.chan@liferay.com>
2012-10-31 15:55:27 -0700

    LPS-30772 Source formatting

--- portal-service/src/com/liferay/portal/kernel/io/Serializer.java
+++ portal-service/src/com/liferay/portal/kernel/io/Serializer.java
@@ -126,6 +126,7 @@ public class Serializer {
 		}
 		else if (serializable instanceof Class) {
 			Class<?> clazz = (Class<?>)serializable;
+
 			ClassLoader classLoader = clazz.getClassLoader();
 
 			String contextName = ClassLoaderPool.getContextName(classLoader);
