51f1160942c2f621eb9fd50b43f74ee74d0bf8a4
Hugo Huijser <hugo.huijser@liferay.com>
2012-11-26 12:41:27 -0800

    LPS-30885 Source formatting

--- portal-impl/src/com/liferay/portal/scheduler/ClusterSchedulerEngine.java
+++ portal-impl/src/com/liferay/portal/scheduler/ClusterSchedulerEngine.java
@@ -566,8 +566,7 @@ public class ClusterSchedulerEngine
 
 			return object;
 		}
-
-		catch(Exception e) {
+		catch (Exception e) {
 			throw new SchedulerException(
 				"Unable to deserialize object from " + string, e);
 		}
@@ -575,8 +574,7 @@ public class ClusterSchedulerEngine
 			try {
 				objectInputStream.close();
 			}
-
-			catch(Exception e) {
+			catch (Exception e) {
 			}
 		}
 	}
