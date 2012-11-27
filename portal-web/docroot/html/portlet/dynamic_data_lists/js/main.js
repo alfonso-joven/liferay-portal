69d8499d1143ea44dbd4b84cabaf34dbdb8e0907
matthewkong <matthew.kong@liferay.com>
2012-11-27 14:19:32 -0800

    LPS-31257 Dynamic Data List global Document and Media link incorrect

--- portal-web/docroot/html/portlet/dynamic_data_lists/js/main.js
+++ portal-web/docroot/html/portlet/dynamic_data_lists/js/main.js
@@ -80,7 +80,7 @@ AUI.add(
 						);
 					},
 
-					_selectFileEntry: function(url, uuid, title, version) {
+					_selectFileEntry: function(groupId, url, uuid, title, version) {
 						var instance = this;
 
 						instance.selectedTitle = title;
@@ -90,7 +90,7 @@ AUI.add(
 							'value',
 							JSON.stringify(
 								{
-									groupId: themeDisplay.getScopeGroupId(),
+									groupId: groupId,
 									uuid: uuid,
 									title: title,
 									version: version
