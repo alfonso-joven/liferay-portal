7cbdb63595261d3e47e49168abea11c24f18c83d
Brian Chan <brian.chan@liferay.com>
2012-11-28 10:41:21 -0800

    LPS-31171 Source formatting

--- portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
@@ -1410,9 +1410,9 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 
 		boolean legacyURL = true;
 
-		String contextPath = PortalUtil.getPathContext();
-
 		while (true) {
+			String contextPath = PortalUtil.getPathContext();
+
 			currentLocation = content.lastIndexOf(
 				contextPath.concat("/c/document_library/get_file?"), beginPos);
 
@@ -1605,6 +1605,8 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 					continue;
 				}
 
+				beginPos = currentLocation;
+
 				DLPortletDataHandlerImpl.exportFileEntry(
 					portletDataContext, dlFileEntryTypesElement,
 					dlFoldersElement, dlFileEntriesElement, dlFileRanksElement,
@@ -1633,8 +1635,6 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 				dlReferenceElement.addAttribute("path", path);
 
 				String dlReference = "[$dl-reference=" + path + "$]";
-
-				beginPos = currentLocation;
 
 				sb.replace(beginPos, endPos, dlReference);
 			}
