18343fd6fd0d9167f8bb0a533cd0b6ad918aad5c
Jonathan Mak <jonathan.mak@liferay.com>
2012-12-06 13:10:54 -0800

    LPS-31327 - Source Formatting

--- portal-web/docroot/html/js/liferay/util_window.js
+++ portal-web/docroot/html/js/liferay/util_window.js
@@ -132,9 +132,7 @@ AUI.add(
 				Liferay.after(
 					'hashChange',
 					function(event) {
-						if (event.uri.indexOf('#') > -1) {
-							dialog.iframe.set('uri', event.uri);
-						}
+						dialog.iframe.set('uri', event.uri);
 					}
 				);
 
