e75ba4e1affe6810bd333b033ca7be24d661c1b9
Brian Chan <brian.chan@liferay.com>
2012-12-11 22:02:50 -0800

    LPS-31535 Source formatting

--- portal-impl/src/com/liferay/portal/search/SearchPermissionCheckerImpl.java
+++ portal-impl/src/com/liferay/portal/search/SearchPermissionCheckerImpl.java
@@ -60,7 +60,6 @@ import com.liferay.portal.service.UserGroupRoleLocalServiceUtil;
 import com.liferay.portal.util.PropsValues;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -227,22 +226,23 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 		throws Exception {
 
 		Group group = null;
-		List<Team> teams = Collections.emptyList();
 
 		if (groupId > 0) {
 			group = GroupLocalServiceUtil.getGroup(groupId);
-
-			teams = TeamLocalServiceUtil.getGroupTeams(groupId);
 		}
 
 		List<Role> roles = ResourceActionsUtil.getRoles(
 			companyId, group, className, null);
 
-		for (Team team : teams) {
-			Role role = RoleLocalServiceUtil.getTeamRole(
-				team.getCompanyId(), team.getTeamId());
+		if (groupId > 0) {
+			List<Team> teams = TeamLocalServiceUtil.getGroupTeams(groupId);
 
-			roles.add(role);
+			for (Team team : teams) {
+				Role role = RoleLocalServiceUtil.getTeamRole(
+					team.getCompanyId(), team.getTeamId());
+
+				roles.add(role);
+			}
 		}
 
 		long[] roleIdsArray = new long[roles.size()];
