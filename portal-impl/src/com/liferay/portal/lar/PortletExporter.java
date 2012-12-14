289f22301e09c6b9035bba6d2c04e59055e32f45
KocsisDaniel <daniel.kocsis@liferay.com>
2012-12-14 01:48:12 +0100

    LPS-31002 Get strict but not layout based portlet preferences when export

--- portal-impl/src/com/liferay/portal/lar/PortletExporter.java
+++ portal-impl/src/com/liferay/portal/lar/PortletExporter.java
@@ -810,7 +810,7 @@ public class PortletExporter {
 
 		if (exportPortletData) {
 			javax.portlet.PortletPreferences jxPreferences =
-				PortletPreferencesFactoryUtil.getStrictLayoutPortletSetup(
+				PortletPreferencesFactoryUtil.getStrictPortletSetup(
 					layout, portletId);
 
 			if (!portlet.isPreferencesUniquePerLayout()) {
