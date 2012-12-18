93d2fe560c71b2a5d1931c2b88316f42ec734e6e
Brian Chan <brian.chan@liferay.com>
2012-12-18 11:52:00 -0800

    LPS-30480 Source formatting

--- portal-impl/src/com/liferay/portlet/documentlibrary/action/GetFileAction.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/action/GetFileAction.java
@@ -230,7 +230,7 @@ public class GetFileAction extends PortletAction {
 
 		if (Validator.isNull(version)) {
 			if ((fileEntry != null) &&
-			    Validator.isNotNull(fileEntry.getVersion())) {
+				Validator.isNotNull(fileEntry.getVersion())) {
 
 				version = fileEntry.getVersion();
 			}
