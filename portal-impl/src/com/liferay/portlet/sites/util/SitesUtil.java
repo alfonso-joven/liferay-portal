07b4033fad2affd4a648c8fccd6f9642d73621d0
Hugo Huijser <hugo.huijser@liferay.com>
2013-01-07 11:32:49 -0800

    LPS-31259 Deprecate method with typo

--- portal-impl/src/com/liferay/portlet/sites/util/SitesUtil.java
+++ portal-impl/src/com/liferay/portlet/sites/util/SitesUtil.java
@@ -786,6 +786,15 @@ public class SitesUtil {
 		doMergeLayoutPrototypeLayout(group, layout);
 	}
 
+	/**
+	 * @deprecated {@link #mergeLayoutPrototypeLayout(Group, Layout)}
+	 */
+	public static void mergeLayoutProtypeLayout(Group group, Layout layout)
+		throws Exception {
+
+		mergeLayoutPrototypeLayout(group, layout);
+	}
+
 	public static void mergeLayoutSetPrototypeLayouts(
 			Group group, LayoutSet layoutSet)
 		throws Exception {
@@ -910,6 +919,16 @@ public class SitesUtil {
 				LayoutLocalServiceVirtualLayoutsAdvice.class.getName(),
 				String.valueOf(layoutSet.getLayoutSetId()), owner, false);
 		}
+	}
+
+	/**
+	 * @deprecated {@link #mergeLayoutSetPrototypeLayouts(Group, LayoutSet)}
+	 */
+	public static void mergeLayoutSetProtypeLayouts(
+			Group group, LayoutSet layoutSet)
+		throws Exception {
+
+		mergeLayoutSetPrototypeLayouts(group, layoutSet);
 	}
 
 	public static void resetPrototype(Layout layout)
