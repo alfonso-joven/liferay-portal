2ca0d4a8215a960bfc5b37790714840bc12b8af1
eric.min <eric.min@liferay.com>
2013-01-08 15:20:25 +0800

    LPS-32138

--- portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
@@ -2168,7 +2168,7 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 
 			FileEntry fileEntry =
 				DLAppLocalServiceUtil.getFileEntryByUuidAndGroupId(
-					fileEntryUUID, portletDataContext.getGroupId());
+					fileEntryUUID, portletDataContext.getScopeGroupId());
 
 			if (fileEntry == null) {
 				continue;
