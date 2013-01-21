ec77b1f6e5fe770c1cd92450719ad0844d87f86c
Brian Chan <brian.chan@liferay.com>
2013-01-21 12:39:28 -0800

    Revert "LPS-31496"
    This reverts commit 107b8111fd7808459d6d2a66b53f9e8417aa8238.

--- portal-impl/src/com/liferay/portal/action/RenderPortletAction.java
+++ portal-impl/src/com/liferay/portal/action/RenderPortletAction.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.action;
 
 import com.liferay.portal.kernel.portlet.WindowStateFactory;
-import com.liferay.portal.kernel.servlet.taglib.aui.ScriptData;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.Portlet;
@@ -24,9 +23,6 @@ import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.WebKeys;
-import com.liferay.taglib.aui.AUIUtil;
-
-import java.io.PrintWriter;
 
 import javax.portlet.WindowState;
 
@@ -96,16 +92,6 @@ public class RenderPortletAction extends Action {
 		PortalUtil.renderPortlet(
 			servletContext, request, response, portlet, queryString, columnId,
 			new Integer(columnPos), new Integer(columnCount), true);
-
-		if (themeDisplay.isIsolated() || themeDisplay.isStateExclusive()) {
-			ScriptData scriptData = AUIUtil.getScriptData(request);
-
-			if (scriptData != null) {
-				PrintWriter writer = response.getWriter();
-
-				AUIUtil.buildScriptData(writer, request, scriptData);
-			}
-		}
 
 		return null;
 	}
