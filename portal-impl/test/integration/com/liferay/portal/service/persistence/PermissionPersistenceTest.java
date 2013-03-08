b50d809ba00a27cb0405807e08324bff5f7dfebe
Brian Chan <brian.chan@liferay.com>
2013-03-07 18:18:43 -0800

    LPS-33564 Regen

--- portal-impl/test/integration/com/liferay/portal/service/persistence/PermissionPersistenceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/persistence/PermissionPersistenceTest.java
@@ -16,6 +16,7 @@ package com.liferay.portal.service.persistence;
 
 import com.liferay.portal.NoSuchPermissionException;
 import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
@@ -23,6 +24,7 @@ import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.util.IntegerWrapper;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Permission;
 import com.liferay.portal.model.impl.PermissionModelImpl;
@@ -167,6 +169,26 @@ public class PermissionPersistenceTest {
 		Permission missingPermission = _persistence.fetchByPrimaryKey(pk);
 
 		Assert.assertNull(missingPermission);
+	}
+
+	@Test
+	public void testActionableDynamicQuery() throws Exception {
+		final IntegerWrapper count = new IntegerWrapper();
+
+		ActionableDynamicQuery actionableDynamicQuery = new PermissionActionableDynamicQuery() {
+				@Override
+				protected void performAction(Object object) {
+					Permission permission = (Permission)object;
+
+					Assert.assertNotNull(permission);
+
+					count.increment();
+				}
+			};
+
+		actionableDynamicQuery.performActions();
+
+		Assert.assertEquals(count.getValue(), _persistence.countAll());
 	}
 
 	@Test
