75f2381755fcd0f7c895e8ff1b424d6a4816e66a
Brian Chan <brian.chan@liferay.com>
2013-03-14 23:03:37 -0700

    LPS-33329 SF

--- portal-impl/src/com/liferay/portal/bean/BeanLocatorImpl.java
+++ portal-impl/src/com/liferay/portal/bean/BeanLocatorImpl.java
@@ -125,6 +125,11 @@ public class BeanLocatorImpl implements BeanLocator {
 		return _applicationContext.getBean(name);
 	}
 
+	/**
+	 * @see {@link
+	 *      com.liferay.portal.security.lang.DoPrivilegedFactory#_getInterfaces(
+	 *      List, Class)}
+	 */
 	protected void getInterfaces(
 		List<Class<?>> interfaceClasses, Class<?> clazz) {
 
@@ -138,6 +143,11 @@ public class BeanLocatorImpl implements BeanLocator {
 		}
 	}
 
+	/**
+	 * @see {@link
+	 *      com.liferay.portal.security.lang.DoPrivilegedFactory#_getInterfaces(
+	 *      Object)}
+	 */
 	protected Class<?>[] getInterfaces(Object object) {
 		List<Class<?>> interfaceClasses = new ArrayList<Class<?>>();
 
