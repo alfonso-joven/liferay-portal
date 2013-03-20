f6bfdf784b571ea7b692c3b4b2215e5b03b5b32e
Brian Chan <brian.chan@liferay.com>
2013-03-20 15:35:29 -0700

    LPS-33899 SF

--- portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAutoProxyCreator.java
+++ portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAutoProxyCreator.java
@@ -76,7 +76,9 @@ public class ServiceBeanAutoProxyCreator
 	}
 
 	private static final String _FINDER_SUFFIX = "Finder";
+
 	private static final String _PERSISTENCE_SUFFIX = "Persistence";
+
 	private static final String _SERVICE_SUFFIX = "Service";
 
 	private MethodInterceptor _methodInterceptor;
