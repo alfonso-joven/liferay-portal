2b3704c303d5a10f2478b903645881119af752e0
Ken Duenwald <ken.duenwald@liferay.com>
2013-03-27 14:58:03 -0700

    SOS-1477

--- portal-web/test/functional/com/liferay/portalweb/socialoffice/users/sites/assignmembersousersites/AssignMemberSOUserSitesTest.java
+++ portal-web/test/functional/com/liferay/portalweb/socialoffice/users/sites/assignmembersousersites/AssignMemberSOUserSitesTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portalweb.socialoffice.users.organizations.assignorganizationsosites;
+package com.liferay.portalweb.socialoffice.users.sites.assignmembersousersites;
 
 import com.liferay.portalweb.portal.BaseTestCase;
 import com.liferay.portalweb.portal.util.RuntimeVariables;
@@ -20,8 +20,8 @@ import com.liferay.portalweb.portal.util.RuntimeVariables;
 /**
  * @author Brian Wing Shun Chan
  */
-public class AssignMembersSOUserOrganizationTest extends BaseTestCase {
-	public void testAssignMembersSOUserOrganization() throws Exception {
+public class AssignMemberSOUserSitesTest extends BaseTestCase {
+	public void testAssignMemberSOUserSites() throws Exception {
 		int label = 1;
 
 		while (label >= 1) {
@@ -37,30 +37,40 @@ public class AssignMembersSOUserOrganizationTest extends BaseTestCase {
 				selenium.clickAt("link=Control Panel",
 					RuntimeVariables.replace("Control Panel"));
 				selenium.waitForPageToLoad("30000");
-				selenium.clickAt("link=Users and Organizations",
-					RuntimeVariables.replace("Users and Organizations"));
+				selenium.clickAt("link=Sites", RuntimeVariables.replace("Sites"));
 				selenium.waitForPageToLoad("30000");
-				selenium.type("//input[@name='_125_keywords']",
-					RuntimeVariables.replace("Organization"));
+				selenium.type("//input[@id='_134_name']",
+					RuntimeVariables.replace("Site "));
 				selenium.clickAt("//input[@value='Search']",
 					RuntimeVariables.replace("Search"));
 				selenium.waitForPageToLoad("30000");
-				Thread.sleep(1000);
+				assertEquals(RuntimeVariables.replace("Site Name"),
+					selenium.getText("//tr[3]/td[1]/a"));
 				assertEquals(RuntimeVariables.replace("Actions"),
 					selenium.getText(
 						"//span[@title='Actions']/ul/li/strong/a/span"));
 				selenium.clickAt("//span[@title='Actions']/ul/li/strong/a/span",
 					RuntimeVariables.replace("Actions"));
 				selenium.waitForVisible(
-					"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Assign Users')]");
-				assertEquals(RuntimeVariables.replace("Assign Users"),
+					"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Manage Memberships')]/a");
+				assertEquals(RuntimeVariables.replace("Manage Memberships"),
 					selenium.getText(
-						"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Assign Users')]"));
+						"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Manage Memberships')]/a"));
 				selenium.click(RuntimeVariables.replace(
-						"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Assign Users')]"));
+						"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'Manage Memberships')]/a"));
 				selenium.waitForPageToLoad("30000");
-				selenium.clickAt("link=Available",
-					RuntimeVariables.replace("Available"));
+				assertEquals(RuntimeVariables.replace("Add Members"),
+					selenium.getText(
+						"//span[@title='Add Members']/ul/li/strong/a/span"));
+				selenium.clickAt("//span[@title='Add Members']/ul/li/strong/a/span",
+					RuntimeVariables.replace("Add Members"));
+				selenium.waitForVisible(
+					"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'User')]/a");
+				assertEquals(RuntimeVariables.replace("User"),
+					selenium.getText(
+						"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'User')]/a"));
+				selenium.click(RuntimeVariables.replace(
+						"//div[@class='lfr-component lfr-menu-list']/ul/li[contains(.,'User')]/a"));
 				selenium.waitForPageToLoad("30000");
 
 				boolean basicVisible = selenium.isVisible("link=\u00ab Basic");
@@ -75,31 +85,32 @@ public class AssignMembersSOUserOrganizationTest extends BaseTestCase {
 					RuntimeVariables.replace("\u00ab Basic"));
 
 			case 2:
-				selenium.waitForVisible("//input[@name='_125_keywords']");
-				selenium.type("//input[@name='_125_keywords']",
+				selenium.type("//input[@name='_174_keywords']",
 					RuntimeVariables.replace("socialoffice01"));
 				selenium.clickAt("//input[@value='Search']",
 					RuntimeVariables.replace("Search"));
 				selenium.waitForPageToLoad("30000");
-				assertFalse(selenium.isChecked("//input[@name='_125_rowIds']"));
-				selenium.clickAt("//input[@name='_125_rowIds']",
-					RuntimeVariables.replace("Select User"));
-				assertTrue(selenium.isChecked("//input[@name='_125_rowIds']"));
-				selenium.clickAt("//input[@value='Update Associations']",
-					RuntimeVariables.replace("Update Associations"));
+				assertEquals(RuntimeVariables.replace(
+						"Social01 Office01 User01"),
+					selenium.getText("//tr[3]/td[2]"));
+				assertFalse(selenium.isChecked("//input[@name='_174_rowIds']"));
+				selenium.clickAt("//input[@name='_174_rowIds']",
+					RuntimeVariables.replace("Entry Check Box"));
+				assertTrue(selenium.isChecked("//input[@name='_174_rowIds']"));
+				selenium.clickAt("//input[@value='Save']",
+					RuntimeVariables.replace("Save"));
 				selenium.waitForPageToLoad("30000");
 				assertEquals(RuntimeVariables.replace(
 						"Your request completed successfully."),
 					selenium.getText("//div[@class='portlet-msg-success']"));
-				selenium.clickAt("link=Current",
-					RuntimeVariables.replace("Current"));
+				selenium.type("//input[@name='_174_keywords']",
+					RuntimeVariables.replace("socialoffice01"));
+				selenium.clickAt("//input[@value='Search']",
+					RuntimeVariables.replace("Search"));
 				selenium.waitForPageToLoad("30000");
-				assertTrue(selenium.isChecked("//td/input"));
 				assertEquals(RuntimeVariables.replace(
 						"Social01 Office01 User01"),
-					selenium.getText("//tr[3]/td[2]"));
-				assertEquals(RuntimeVariables.replace("socialoffice01"),
-					selenium.getText("//tr[3]/td[3]"));
+					selenium.getText("//tr[3]/td[1]"));
 
 			case 100:
 				label = -1;
--- portal-web/test/functional/com/liferay/portalweb/socialoffice/users/usergroups/assignmemberugsousergroupuser/AssignMemberUGSOUserGroupUserTests.java
+++ portal-web/test/functional/com/liferay/portalweb/socialoffice/users/usergroups/assignmemberugsousergroupuser/AssignMemberUGSOUserGroupUserTests.java
@@ -12,24 +12,22 @@
  * details.
  */
 
-package com.liferay.portalweb.socialoffice.users.organizations.assignsoroleorganizationsoconfiguration;
+package com.liferay.portalweb.socialoffice.users.usergroups.assignmemberugsousergroupuser;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
-import com.liferay.portalweb.portal.controlpanel.organizations.organization.addorganization.AddOrganizationTest;
-import com.liferay.portalweb.portal.controlpanel.organizations.organization.addorganization.TearDownOrganizationTest;
-import com.liferay.portalweb.socialoffice.users.organizations.assignmemberssouserorganization.AssignMembersSOUserOrganizationTest;
+import com.liferay.portalweb.portal.controlpanel.usergroups.ugusergroup.addugusergroup.AddUGUserGroupTest;
+import com.liferay.portalweb.portal.controlpanel.usergroups.ugusergroup.addugusergroup.TearDownUGUserGroupTest;
 import com.liferay.portalweb.socialoffice.users.user.addsouser.AddSOUserTest;
 import com.liferay.portalweb.socialoffice.users.user.addsouser.TearDownSOUserTest;
 import com.liferay.portalweb.socialoffice.users.user.editsouserpassword.EditSOUserPasswordTest;
-import com.liferay.portalweb.socialoffice.users.user.removeregularrolessouser.SOUs_ViewMyPrivatePagesNoSORoleTest;
-import com.liferay.portalweb.socialoffice.users.user.removeregularrolessouser.SOUs_ViewMyPublicPagesNoSORoleTest;
-import com.liferay.portalweb.socialoffice.users.user.selectregularrolessouser.SOUs_ViewMyProfileTest;
-import com.liferay.portalweb.socialoffice.users.user.selectregularrolessouser.SOUs_ViewSitesDirectoryTest;
-import com.liferay.portalweb.socialoffice.users.user.selectregularrolessouser.SOUs_ViewWelcomePageTest;
 import com.liferay.portalweb.socialoffice.users.user.signinso.SOUs_SignInSOTest;
 import com.liferay.portalweb.socialoffice.users.user.signinso.SOUs_SignOutSOTest;
 import com.liferay.portalweb.socialoffice.users.user.signinso.SignInSOTest;
 import com.liferay.portalweb.socialoffice.users.user.signinso.SignOutSOTest;
+import com.liferay.portalweb.socialoffice.users.usergroups.assignrolesousergroup.AssignRoleSOUserGroupTest;
+import com.liferay.portalweb.socialoffice.users.usergroups.assignrolesousergroup.RemoveRoleSOUserGroupTest;
+import com.liferay.portalweb.socialoffice.users.usergroups.assignrolesousergroup.SOUs_ViewSODashboardNoTest;
+import com.liferay.portalweb.socialoffice.users.usergroups.assignrolesousergroup.SOUs_ViewSODashboardTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -37,36 +35,38 @@ import junit.framework.TestSuite;
 /**
  * @author Brian Wing Shun Chan
  */
-public class AssignSORoleOrganizationSOConfigurationTests extends BaseTestSuite {
+public class AssignMemberUGSOUserGroupUserTests extends BaseTestSuite {
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
 		testSuite.addTestSuite(AddSOUserTest.class);
 		testSuite.addTestSuite(EditSOUserPasswordTest.class);
+		testSuite.addTestSuite(AddUGUserGroupTest.class);
+		testSuite.addTestSuite(AssignMemberUGSOUserGroupUserTest.class);
 		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOUs_SignInSOTest.class);
-		testSuite.addTestSuite(SOUs_ViewMyPublicPagesNoSORoleTest.class);
-		testSuite.addTestSuite(SOUs_ViewMyPrivatePagesNoSORoleTest.class);
+		testSuite.addTestSuite(SOUs_ViewSODashboardNoTest.class);
 		testSuite.addTestSuite(SOUs_SignOutSOTest.class);
 		testSuite.addTestSuite(SignInSOTest.class);
-		testSuite.addTestSuite(AddOrganizationTest.class);
-		testSuite.addTestSuite(AssignSORoleOrganizationSOConfigurationTest.class);
-		testSuite.addTestSuite(AssignMembersSOUserOrganizationTest.class);
+		testSuite.addTestSuite(AssignRoleSOUserGroupTest.class);
 		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOUs_SignInSOTest.class);
-		testSuite.addTestSuite(SOUs_ViewWelcomePageTest.class);
-		testSuite.addTestSuite(SOUs_ViewSitesDirectoryTest.class);
-		testSuite.addTestSuite(SOUs_ViewMyProfileTest.class);
+		testSuite.addTestSuite(SOUs_ViewSODashboardTest.class);
 		testSuite.addTestSuite(SOUs_SignOutSOTest.class);
 		testSuite.addTestSuite(SignInSOTest.class);
-		testSuite.addTestSuite(RemoveSORoleOrganizationSOConfigurationTest.class);
+		testSuite.addTestSuite(RemoveRoleSOUserGroupTest.class);
 		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOUs_SignInSOTest.class);
-		testSuite.addTestSuite(SOUs_ViewMyPrivatePagesNoSORoleTest.class);
-		testSuite.addTestSuite(SOUs_ViewMyPublicPagesNoSORoleTest.class);
+		testSuite.addTestSuite(SOUs_ViewSODashboardNoTest.class);
+		testSuite.addTestSuite(SOUs_SignOutSOTest.class);
+		testSuite.addTestSuite(SignInSOTest.class);
+		testSuite.addTestSuite(AssignRoleSOUserGroupTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
+		testSuite.addTestSuite(SOUs_SignInSOTest.class);
+		testSuite.addTestSuite(SOUs_ViewSODashboardTest.class);
 		testSuite.addTestSuite(SOUs_SignOutSOTest.class);
 		testSuite.addTestSuite(SignInSOTest.class);
 		testSuite.addTestSuite(TearDownSOUserTest.class);
-		testSuite.addTestSuite(TearDownOrganizationTest.class);
+		testSuite.addTestSuite(TearDownUGUserGroupTest.class);
 
 		return testSuite;
 	}
