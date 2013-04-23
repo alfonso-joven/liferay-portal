09012b1ad383f32fc9f843fe9906779e889ca858
Brian Chan <brian.chan@liferay.com>
2013-04-22 22:03:27 -0700

    LPS-34667 SF

--- portal-impl/src/com/liferay/portal/service/impl/LayoutSetLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutSetLocalServiceImpl.java
@@ -159,7 +159,8 @@ public class LayoutSetLocalServiceImpl extends LayoutSetLocalServiceBaseImpl {
 			}
 			catch (NoSuchImageException nsie) {
 				if (_log.isWarnEnabled()) {
-					_log.warn("Unable to delete image " + user.getPortraitId());
+					_log.warn(
+						"Unable to delete image " + layoutSet.getLogoId());
 				}
 			}
 		}
