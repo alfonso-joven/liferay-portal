91bfb97a9705c16ca66f94569434b5b0aa0655b0
Dustin Ryerson <dustin.ryerson@liferay.com>
2013-04-23 17:10:15 -0700

    LPS-34263

--- portal-impl/src/com/liferay/portlet/usersadmin/util/OrganizationIndexer.java
+++ portal-impl/src/com/liferay/portlet/usersadmin/util/OrganizationIndexer.java
@@ -88,6 +88,23 @@ public class OrganizationIndexer extends BaseIndexer {
 			return;
 		}
 
+		List<Long> excludedOrganizationIds = (List<Long>)params.get(
+			"excludedOrganizationIds");
+
+		if ((excludedOrganizationIds != null) &&
+			!excludedOrganizationIds.isEmpty()) {
+
+			BooleanQuery booleanQuery = BooleanQueryFactoryUtil.create(
+				searchContext);
+
+			for (long excludedOrganizationId : excludedOrganizationIds) {
+				booleanQuery.addTerm(
+					"organizationId", String.valueOf(excludedOrganizationId));
+			}
+
+			contextQuery.add(booleanQuery, BooleanClauseOccur.MUST_NOT);
+		}
+
 		List<Organization> organizationsTree = (List<Organization>)params.get(
 			"organizationsTree");
 
@@ -103,6 +120,15 @@ public class OrganizationIndexer extends BaseIndexer {
 
 			contextQuery.add(booleanQuery, BooleanClauseOccur.MUST);
 		}
+		else {
+			long parentOrganizationId = GetterUtil.getLong(
+				searchContext.getAttribute("parentOrganizationId"));
+
+			if (parentOrganizationId > 0) {
+				contextQuery.addRequiredTerm(
+					"parentOrganizationId", parentOrganizationId);
+			}
+		}
 	}
 
 	@Override
@@ -113,8 +139,6 @@ public class OrganizationIndexer extends BaseIndexer {
 		addSearchTerm(searchQuery, searchContext, "city", false);
 		addSearchTerm(searchQuery, searchContext, "country", false);
 		addSearchTerm(searchQuery, searchContext, "name", false);
-		addSearchTerm(
-			searchQuery, searchContext, "parentOrganizationId", false);
 		addSearchTerm(searchQuery, searchContext, "region", false);
 		addSearchTerm(searchQuery, searchContext, "street", false);
 		addSearchTerm(searchQuery, searchContext, "type", false);
--- portal-web/docroot/html/portlet/directory/view_organizations.jsp
+++ portal-web/docroot/html/portlet/directory/view_organizations.jsp
@@ -18,6 +18,12 @@
 
 <%
 PortletURL portletURL = (PortletURL)request.getAttribute("view.jsp-portletURL");
+
+long parentOrganizationId = ParamUtil.getLong(request, "parentOrganizationId", OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID);
+
+if (parentOrganizationId > 0) {
+	portletURL.setParameter("parentOrganizationId", String.valueOf(parentOrganizationId));
+}
 %>
 
 <liferay-ui:search-container
@@ -34,10 +40,26 @@ PortletURL portletURL = (PortletURL)request.getAttribute("view.jsp-portletURL");
 
 	LinkedHashMap<String, Object> organizationParams = new LinkedHashMap<String, Object>();
 
-	long parentOrganizationId = ParamUtil.getLong(request, "parentOrganizationId", OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID);
-
 	if (parentOrganizationId <= 0) {
 		parentOrganizationId = OrganizationConstants.ANY_PARENT_ORGANIZATION_ID;
+	}
+
+	if (Validator.isNotNull(searchTerms.getKeywords()) || searchTerms.isAdvancedSearch()) {
+		if (parentOrganizationId != OrganizationConstants.ANY_PARENT_ORGANIZATION_ID) {
+			List<Long> excludedOrganizationIds = new ArrayList<Long>();
+
+			excludedOrganizationIds.add(parentOrganizationId);
+
+			organizationParams.put("excludedOrganizationIds", excludedOrganizationIds);
+
+			Organization parentOrganization = OrganizationLocalServiceUtil.getOrganization(parentOrganizationId);
+
+			List<Organization> organizations = new ArrayList<Organization>();
+
+			organizations.add(parentOrganization);
+
+			organizationParams.put("organizationsTree", organizations);
+		}
 	}
 	%>
 
