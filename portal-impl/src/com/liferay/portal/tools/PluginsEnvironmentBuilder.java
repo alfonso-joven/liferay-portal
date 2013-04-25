d5f01154bb2cffc2b7116d7e855c6a07f5dbf25c
Brian Chan <brian.chan@liferay.com>
2013-04-25 15:40:05 -0700

    LPS-34598 The osgi stuff is not needed, will put them all in lib/* so will piggy back off of existing shared logic

--- portal-impl/src/com/liferay/portal/tools/PluginsEnvironmentBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/PluginsEnvironmentBuilder.java
@@ -126,22 +126,6 @@ public class PluginsEnvironmentBuilder {
 		sb.append("\t\t</attributes>\n\t</classpathentry>\n");
 	}
 
-	protected void addOsgiClasspathEntries(StringBundler sb, String projectDir)
-		throws Exception {
-
-		DirectoryScanner directoryScanner = new DirectoryScanner();
-
-		directoryScanner.setBasedir(projectDir + "/lib/");
-		directoryScanner.setExcludes(new String[] {"*-shared-*.jar"});
-		directoryScanner.setIncludes(new String[] {"**\\*.jar"});
-
-		directoryScanner.scan();
-
-		for (String file : directoryScanner.getIncludedFiles()) {
-			addClasspathEntry(sb, "lib/" + file);
-		}
-	}
-
 	protected List<String> getCommonJars() {
 		List<String> jars = new ArrayList<String>();
 
@@ -492,8 +476,6 @@ public class PluginsEnvironmentBuilder {
 				addClasspathEntry(sb, "lib/" + jar);
 			}
 		}
-
-		addOsgiClasspathEntries(sb, projectDirName);
 
 		sb.append("\t<classpathentry kind=\"output\" path=\"bin\" />\n");
 		sb.append("</classpath>");
