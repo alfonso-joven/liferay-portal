e0dd351fc0734e57768e07f17b173fd6ed41fd4a
Brian Chan <brian.chan@liferay.com>
2013-05-23 22:10:24 -0700

    LPS-35131 Regen 2

--- util-taglib/src/com/liferay/taglib/util/ParamAndPropertyAncestorTagImpl.java
+++ util-taglib/src/com/liferay/taglib/util/ParamAndPropertyAncestorTagImpl.java
@@ -35,6 +35,7 @@ public class ParamAndPropertyAncestorTagImpl
 	extends BaseBodyTagSupport
 	implements ParamAncestorTag, PropertyAncestorTag {
 
+	@Override
 	public void addParam(String name, String value) {
 		if (_params == null) {
 			_params = new LinkedHashMap<String, String[]>();
@@ -72,6 +73,7 @@ public class ParamAndPropertyAncestorTagImpl
 		_params.put(name, values);
 	}
 
+	@Override
 	public void addProperty(String name, String value) {
 		if (_properties == null) {
 			_properties = new LinkedHashMap<String, String[]>();
