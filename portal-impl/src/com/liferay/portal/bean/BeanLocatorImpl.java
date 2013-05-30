e4ebbfa6b87f7c866ac19957fe2be42092bf7ee2
Raymond Auge <raymond.auge@liferay.com>
2013-05-30 18:25:34 -0400

    LPS-35815 More than one PACL proxies are applied to certain beans

--- portal-impl/src/com/liferay/portal/bean/BeanLocatorImpl.java
+++ portal-impl/src/com/liferay/portal/bean/BeanLocatorImpl.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.security.pacl.DoPrivileged;
 import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
 import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.kernel.util.ReflectionUtil;
+import com.liferay.portal.security.lang.DoPrivilegedBean;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -148,6 +149,10 @@ public class BeanLocatorImpl implements BeanLocator {
 		}
 
 		if (bean == null) {
+			return bean;
+		}
+
+		if (bean instanceof DoPrivilegedBean) {
 			return bean;
 		}
 
