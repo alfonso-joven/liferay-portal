1461f57086efe5acef642a430983e82960a1a333
Brian Chan <brian.chan@liferay.com>
2013-05-30 18:58:39 -0700

    Source formatting - a class shouldn't need to reference itself to call static methods on itself

--- portal-service/src/com/liferay/portal/kernel/util/Validator.java
+++ portal-service/src/com/liferay/portal/kernel/util/Validator.java
@@ -684,7 +684,7 @@ public class Validator {
 	}
 
 	public static boolean isFilePath(String path, boolean isParentDirAllowed) {
-		if (Validator.isNull(path)) {
+		if (isNull(path)) {
 			return false;
 		}
 
