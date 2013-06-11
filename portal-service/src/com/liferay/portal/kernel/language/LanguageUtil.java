405ab3b520156986c440c4dfb61203130d2685ba
vilmospapp <vilmos.papp@liferay.com>
2013-06-11 16:23:17 +0200

    LPS-35920 Added new  method to determine whether a key is exists for the specified Locale or not

--- portal-service/src/com/liferay/portal/kernel/language/LanguageUtil.java
+++ portal-service/src/com/liferay/portal/kernel/language/LanguageUtil.java
@@ -15,6 +15,8 @@
 package com.liferay.portal.kernel.language;
 
 import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Locale;
 
@@ -261,6 +263,12 @@ public class LanguageUtil {
 
 	public static boolean isDuplicateLanguageCode(String languageCode) {
 		return getLanguage().isDuplicateLanguageCode(languageCode);
+	}
+
+	public static boolean isValidLanguageKey(Locale locale, String key) {
+		String value = getLanguage().get(locale, key, StringPool.BLANK);
+
+		return Validator.isNotNull(value);
 	}
 
 	public static void resetAvailableLocales(long companyId) {
