1e20e157882fd30c44207dab047d561fccd6df83
Hugo Huijser <hugo.huijser@liferay.com>
2013-06-24 11:29:12 -0700

    LPS-36179 Source formatting

--- util-java/src/com/liferay/util/dao/orm/CustomSQL.java
+++ util-java/src/com/liferay/util/dao/orm/CustomSQL.java
@@ -194,7 +194,7 @@ public class CustomSQL {
 		}
 
 		if (isVendorMySQL() || isVendorOracle()) {
-			keywords = StringUtil.replace(keywords,"_","\\_");
+			keywords = StringUtil.replace(keywords, "_", "\\_");
 		}
 
 		if (lowerCase) {
