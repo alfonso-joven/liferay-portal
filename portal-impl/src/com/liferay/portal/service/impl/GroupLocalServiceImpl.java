d1bb2613c21419f390b60e179c589868a2de6d06
Brian Chan <brian.chan@liferay.com>
2013-07-15 13:54:37 -0700

    LPS-35570 Use helper class

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -3498,7 +3498,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 			name = StringUtil.replace(
 				name, StringPool.PERCENT, StringPool.BLANK);
 
-			if (companyName.toLowerCase().contains(name.toLowerCase())) {
+			if (StringUtil.matchesIgnoreCase(companyName, name)) {
 				realName =
 					StringPool.PERCENT + GroupConstants.GUEST.toLowerCase() +
 						StringPool.PERCENT;
