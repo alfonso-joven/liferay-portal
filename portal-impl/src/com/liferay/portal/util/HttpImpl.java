ab02a8feac89a306a37b229e4e52cf441709a0dc
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2013-07-23 17:43:55 +0200

    LPS-25368 SF (Not related to the bug)

--- portal-impl/src/com/liferay/portal/util/HttpImpl.java
+++ portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -343,9 +343,8 @@ public class HttpImpl implements Http {
 		if (isProxyHost(hostConfiguration.getHost())) {
 			return _proxyHttpClient;
 		}
-		else {
-			return _httpClient;
-		}
+
+		return _httpClient;
 	}
 
 	@Override
@@ -402,7 +401,7 @@ public class HttpImpl implements Http {
 		if (Validator.isNull(url)) {
 			return url;
 		}
-		
+
 		url = removeProtocol(url);
 
 		int pos = url.indexOf(CharPool.SLASH);
@@ -410,9 +409,8 @@ public class HttpImpl implements Http {
 		if (pos != -1) {
 			return url.substring(0, pos);
 		}
-		else {
-			return url;
-		}
+
+		return url;
 	}
 
 	/**
@@ -548,9 +546,8 @@ public class HttpImpl implements Http {
 		if (pos == -1) {
 			return url;
 		}
-		else {
-			return url.substring(0, pos);
-		}
+
+		return url.substring(0, pos);
 	}
 
 	@Override
@@ -563,9 +560,8 @@ public class HttpImpl implements Http {
 		if (!secure) {
 			return Http.HTTP;
 		}
-		else {
-			return Http.HTTPS;
-		}
+
+		return Http.HTTPS;
 	}
 
 	@Override
@@ -585,9 +581,8 @@ public class HttpImpl implements Http {
 		if (pos != -1) {
 			return url.substring(0, pos);
 		}
-		else {
-			return Http.HTTP;
-		}
+
+		return Http.HTTP;
 	}
 
 	@Override
@@ -601,9 +596,8 @@ public class HttpImpl implements Http {
 		if (pos == -1) {
 			return StringPool.BLANK;
 		}
-		else {
-			return url.substring(pos + 1);
-		}
+
+		return url.substring(pos + 1);
 	}
 
 	@Override
@@ -616,6 +610,7 @@ public class HttpImpl implements Http {
 		if (Validator.isNull(url)) {
 			return false;
 		}
+
 		return Validator.isNotNull(getDomain(url));
 	}
 
@@ -626,9 +621,8 @@ public class HttpImpl implements Http {
 		if (pos != -1) {
 			return true;
 		}
-		else {
-			return false;
-		}
+
+		return false;
 	}
 
 	@Override
@@ -636,9 +630,8 @@ public class HttpImpl implements Http {
 		if (Validator.isNotNull(_PROXY_HOST) && (_PROXY_PORT > 0)) {
 			return true;
 		}
-		else {
-			return false;
-		}
+
+		return false;
 	}
 
 	@Override
@@ -659,9 +652,8 @@ public class HttpImpl implements Http {
 		if (hasProxyConfig() && !isNonProxyHost(host)) {
 			return true;
 		}
-		else {
-			return false;
-		}
+
+		return false;
 	}
 
 	@Override
@@ -812,9 +804,8 @@ public class HttpImpl implements Http {
 		if (pos > 0) {
 			return url.substring(pos);
 		}
-		else {
-			return url;
-		}
+
+		return url;
 	}
 
 	@Override
@@ -879,6 +870,7 @@ public class HttpImpl implements Http {
 		if (Validator.isNull(url)) {
 			return url;
 		}
+
 		if (url.startsWith(Http.HTTP_WITH_SLASH)) {
 			return url.substring(Http.HTTP_WITH_SLASH.length());
 		}
@@ -1030,9 +1022,8 @@ public class HttpImpl implements Http {
 		if (headers.length == 0) {
 			return false;
 		}
-		else {
-			return true;
-		}
+
+		return true;
 	}
 
 	protected void processPostMethod(
