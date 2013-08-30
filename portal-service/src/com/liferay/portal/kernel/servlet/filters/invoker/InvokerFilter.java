10e2114ca9033e598110f1fe29536513866f88e3
Dustin Ryerson <dustin.ryerson@liferay.com>
2013-08-30 09:10:41 -0700

    LPS-39202 Support for X-Content-Type-Options header, X-Frame-Options header and X-XSS-Protection header

--- portal-service/src/com/liferay/portal/kernel/servlet/filters/invoker/InvokerFilter.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/filters/invoker/InvokerFilter.java
@@ -20,7 +20,7 @@ import com.liferay.portal.kernel.concurrent.ConcurrentLFUCache;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.NonSerializableObjectRequestWrapper;
-import com.liferay.portal.kernel.servlet.SecureHttpServletResponseWrapper;
+import com.liferay.portal.kernel.servlet.SanitizedServletResponse;
 import com.liferay.portal.kernel.util.BasePortalLifecycle;
 import com.liferay.portal.kernel.util.ContextPathUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -69,7 +69,7 @@ public class InvokerFilter extends BasePortalLifecycle implements Filter {
 
 		HttpServletResponse response = (HttpServletResponse)servletResponse;
 
-		response = secureResponseHeaders(response);
+		response = secureResponseHeaders(request, response);
 
 		request.setAttribute(WebKeys.INVOKER_FILTER_URI, uri);
 
@@ -247,10 +247,22 @@ public class InvokerFilter extends BasePortalLifecycle implements Filter {
 	}
 
 	protected HttpServletResponse secureResponseHeaders(
-		HttpServletResponse response) {
+		HttpServletRequest request, HttpServletResponse response) {
 
-		return new SecureHttpServletResponseWrapper(response);
+		if (!GetterUtil.getBoolean(
+				request.getAttribute(_SECURE_RESPONSE), true)) {
+
+			return response;
+		}
+
+		request.setAttribute(_SECURE_RESPONSE, Boolean.FALSE);
+
+		return SanitizedServletResponse.getSanitizedServletResponse(
+			request, response);
 	}
+
+	private static final String _SECURE_RESPONSE =
+		InvokerFilter.class.getName() + "SECURE_RESPONSE";
 
 	private static Log _log = LogFactoryUtil.getLog(InvokerFilter.class);
 
