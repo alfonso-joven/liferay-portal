39b85da39a81940a913445d4a795359e500c512f
Hugo Huijser <hugo.huijser@liferay.com>
2013-09-17 09:54:52 +0800

    LPS-39819 Rename methods

--- portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -2378,7 +2378,7 @@ public class HookHotDeployListener
 		}
 
 		Map<String, LayoutSettings> layoutSettingsMap =
-			LayoutSettings.getCurrentLayoutSettings();
+			LayoutSettings.getLayoutSettingsMap();
 
 		for (String layoutType : layoutSettingsMap.keySet()) {
 			if (!layoutType.equals(LayoutConstants.TYPE_CONTROL_PANEL) &&
@@ -2390,7 +2390,7 @@ public class HookHotDeployListener
 
 		for (String type : value) {
 			if (!layoutSettingsMap.containsKey(type)) {
-				LayoutSettings.setLayoutSetting(type);
+				LayoutSettings.addLayoutSetting(type);
 			}
 		}
 	}
