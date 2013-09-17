2e48317c60c9c9b6a0f6d4b59c1c10cadec51c0b
Hugo Huijser <hugo.huijser@liferay.com>
2013-09-17 11:22:54 +0800

    LPS-23365 Source formatting

--- portal-impl/src/com/liferay/portal/servlet/MainServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/MainServlet.java
@@ -1261,7 +1261,7 @@ public class MainServlet extends ActionServlet {
 				plid = group.getDefaultPublicPlid();
 
 				if ((plid == LayoutConstants.DEFAULT_PLID) ||
-					 group.isStagingGroup()) {
+					group.isStagingGroup()) {
 
 					Group guestGroup = GroupLocalServiceUtil.getGroup(
 						layout.getCompanyId(), GroupConstants.GUEST);
