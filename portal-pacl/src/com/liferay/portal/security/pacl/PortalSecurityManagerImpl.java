948fd640114d3116857df3f8e81399aeced014c3
Brian Chan <brian.chan@liferay.com>
2013-09-17 16:19:31 -0700

    LPS-39137 Fix compile, add @Override

--- portal-pacl/src/com/liferay/portal/security/pacl/PortalSecurityManagerImpl.java
+++ portal-pacl/src/com/liferay/portal/security/pacl/PortalSecurityManagerImpl.java
@@ -541,10 +541,12 @@ public class PortalSecurityManagerImpl extends SecurityManager
 
 	private static class DoDataAccessPACL implements DataAccess.PACL {
 
+		@Override
 		public DataSource getDataSource() {
 			return AccessController.doPrivileged(
 				new PrivilegedAction<DataSource>() {
 
+					@Override
 					public DataSource run() {
 						return InfrastructureUtil.getDataSource();
 					}
@@ -553,6 +555,7 @@ public class PortalSecurityManagerImpl extends SecurityManager
 			);
 		}
 
+		@Override
 		public DataSource getDataSource(final String location)
 			throws NamingException {
 
@@ -560,6 +563,7 @@ public class PortalSecurityManagerImpl extends SecurityManager
 				return AccessController.doPrivileged(
 					new PrivilegedExceptionAction<DataSource>() {
 
+						@Override
 						public DataSource run() throws Exception {
 							Properties properties = PropsUtil.getProperties(
 								PropsKeys.JNDI_ENVIRONMENT, true);
@@ -574,7 +578,7 @@ public class PortalSecurityManagerImpl extends SecurityManager
 				);
 			}
 			catch (PrivilegedActionException pe) {
-				throw (NamingException)e.getException();
+				throw (NamingException)pe.getException();
 			}
 		}
 
